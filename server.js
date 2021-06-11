a0ebaab0285a4888c0330ba8725c5c0acdf465fe
nvduran <nvduran@gmail.com>
2021-06-10 22:22:47 -0500

    sql bugs fixed

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const table = require('console.table');
 const connection = mysql.createConnection(
     {host:'localhost', user: 'root', password: '!6MUOweit', database:'employee_DB'}
 )
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    startPrompt();
+})
+
+const startPrompt = () => {
+    console.log('started');
+}
